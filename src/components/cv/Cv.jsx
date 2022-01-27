c3eff716dbcf1946bd306406279b2f802a25d7a5
Nike <spengler.nike@gmail.com>
2022-01-27 14:18:10 +0100

    update

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -1,17 +1,18 @@
-import "./cv.scss"
+import { useState } from "react";
+import "./cv.scss";
 
 export default function Cv() {
-        const [currentSlide, setCurrentSlide] =useState(0) 
+        const [currentSlide, setCurrentSlide] = useState(0);
     const data = [
         {
             id: "1",
             title: "Utbilning",
-            year1: "2015-pågående",
-            year2: "2014-2020",
-            year3: "2016-2019",
-            desc1: "Frilandsande grafisk formgivare",
-            desc2: "Postmuseum, musievärd",
-            desc3: "Norbergfestivalen, ljud och ljus",
+            year1: "2021-2023",
+            year2: "2017",
+            year3: "2010-2015",
+            desc1: "KYH, Frontend Developer",
+            desc2: "Grafisk Design för webben, HKR 7,5hp",
+            desc3: "Konstfack, Grafisk Design & Illustration",
         },
         {
             id: "2",
@@ -25,24 +26,27 @@ export default function Cv() {
         },
         {
             id: "3",
-            title: "Praktik",
-            year1: "2015-pågående",
-            year2: "2014-2020",
-            year3: "2016-2019",
-            desc1: "Frilandsande grafisk formgivare",
-            desc2: "Postmuseum, musievärd",
-            desc3: "Norbergfestivalen, ljud och ljus",
+            title: "Praktik & Stipendium",
+            year1: "2012",
+            year2: "2012",
+            year3: "2010",
+            desc1: "Torafu Arcitects Tokyo, praktikant",
+            desc2: "Sasakawa Foundation, utbytestermin",
+            desc3: "Grafikens hus, recedency",
         },
     ];
 
+
     const handleClick = (way) => {
-       way  === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide -1 : 2) : setCurrentSlide(currentSlide<data.length -1 ? currentSlide + 1 : 0);
+       way  === "left" 
+       ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) 
+       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
     };
 
     return (
         <div className="cv" id="cv">
             <div className="slider" 
-            style={{transform: `translateX(-${currentSlide * 100}vw)` }} >
+            style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
                 {data.map((d) => (
                 <div className="container">
                     <div className="item">
@@ -70,7 +74,7 @@ export default function Cv() {
                 <img src="assets/down.png" className="arrow-bottom2" alt="" />
             </a>
             <img src="assets/down.png" className="arrow left" alt="" onClick={()=> handleClick("left")} />
-            <img src="assets/down.png" className="arrow right" alt="" onClick={()=> handleClick("")} />
+            <img src="assets/down.png" className="arrow right" alt="" onClick={()=> handleClick()} />
         </div>
-    )
+    );
 }
