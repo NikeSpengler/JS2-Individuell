3058b75a45f3644d934d632ed6f837f410699fdc
Nike <spengler.nike@gmail.com>
2022-01-28 15:27:32 +0100

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,8 +8,12 @@ export default function About() {
                     <div className="item">
                         <div className="left">
                             <div className="leftContainer">
-                                <p className="title">About</p>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe nisi molestias vitae consequatur culpa fugit maiores labore! Dolorem, harum sunt accusantium quisquam consequatur earum ullam illo facere, aut dicta dolorum.</p>
+                                {/* <p className="title">About</p> */}
+                                <p>I’m a Stockholm based Frontend Developer with graphic design experience, often from working in close collaboration with clients. The last few years I’ve shifted from graphic design towards web development which I’ve become rather passionate about. I’m drawn to working with data structures, developing playful visual aesthetics and creating intuitive functionality.</p>
+                                <br />
+                                <br />
+                                <p className="title">Goals</p>
+                                <p>I’m a Stockholm based front-end developer with graphic design experience, often from working in close collaboration with clients. The last few years I’ve shifted from graphic design towards web development which I’ve become rather passionate about. </p>
                             </div>
                         </div>
                         <div className="right">
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,27 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className='contact' id="contact">
-            
+            <div className="left">
+                <img src="assets/mail4.png" height="400px" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact me!</h2>
+                <br />
+                <br />
+                <li><p>spengler.nike@gmail.com</p></li>
+                <li><p>+4673 680 66 24</p></li>
+                <br />
+                <li><a href="#">Github</a></li>
+                <li><a href="#">Linkedin</a></li>
+                <li><a href="#">Instagram</a></li>
+                
+                {/* <form>
+                    <input type="text" placeholder="Email"/>
+                    <textarea name="Message" id="" cols="30" rows="10"></textarea>
+                    <button type="submit">Send</button>
+                </form>  */}
+
+            </div>
         </div>
     )
 }
